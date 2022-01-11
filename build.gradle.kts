dd631332c4b782423378cf12f02f54e152c81a3b
Gavenda <gavenda@disroot.org>
2022-01-12 00:31:21 +0800

    Update to JDA 5.0 alpha 4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
-import java.io.ByteArrayOutputStream
 import java.util.Properties
 
 plugins {
     application
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
 }
 
 repositories {
@@ -12,25 +11,8 @@ repositories {
     maven("https://m2.dv8tion.net/releases")
 }
 
-val botMainClass = "basura.MainKt"
-val gitHash: String get() {
-    if(File(".git").exists().not()) {
-        return "SNAPSHOT"
-    }
-
-    return ByteArrayOutputStream()
-        .use { outputStream ->
-            project.exec {
-                commandLine("git")
-                args("rev-parse", "--short", "HEAD")
-                standardOutput = outputStream
-            }
-            outputStream.toString().trim()
-        }
-}
-
 group = "basura"
-version = "1.0-b$gitHash"
+version = "1.1-SNAPSHOT"
 
 dependencies {
     // Align versions of all Kotlin components
@@ -38,26 +20,26 @@ dependencies {
 
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-native-mt")
 
     // For GraphQL
-    implementation("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
 
     // Jackson
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
     // Dependency Injection
-    implementation("org.kodein.di:kodein-di:7.7.0")
+    implementation("org.kodein.di:kodein-di:7.10.0")
 
     // Discord API
-    implementation("net.dv8tion:JDA:4.3.0_299") {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.4") {
         exclude("club.minnced")
     }
 
     // Logging
-    implementation("org.apache.logging.log4j:log4j-api:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-core:2.14.1")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-core:2.17.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
 
     // HTML to Markdown
     implementation("io.github.furstenheim:copy_down:1.0")
@@ -65,19 +47,19 @@ dependencies {
     // DB
     implementation("org.ktorm:ktorm-core:3.4.1") // ORM
     implementation("com.zaxxer:HikariCP:5.0.0") // Pool
-    implementation("org.postgresql:postgresql:42.2.23") // DB
-    implementation("org.flywaydb:flyway-core:7.14.1") // Migration
+    implementation("org.postgresql:postgresql:42.3.1") // DB
+    implementation("org.flywaydb:flyway-core:8.3.0") // Migration
 }
 
 java {                                      
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks {
     compileKotlin {
         kotlinOptions {
-            jvmTarget = "11"
+            jvmTarget = "17"
             freeCompilerArgs = listOf(
                 "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
                 "-Xuse-experimental=kotlinx.coroutines.DelicateCoroutinesApi",
@@ -105,5 +87,5 @@ tasks {
 }
 
 application {
-    mainClass.set(botMainClass)
+    mainClass.set( "basura.MainKt")
 }
